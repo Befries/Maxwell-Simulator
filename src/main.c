db0a43d82707c3d6debe1010422c58b44c4388ba
Befries <aymeric.dejolier@ulb.be>
2024-06-20 22:43:37 +0200

    feat: build UI skeleton

--- src/main.c
+++ src/main.c
@@ -4,16 +4,17 @@
 static void activate_app(GtkApplication *app, gpointer user_data) {
   
   GtkBuilder *builder = gtk_builder_new ();
-  gtk_builder_add_from_file (builder, "./src/ui/builder.ui", NULL);
+  gtk_builder_add_from_file (builder, "./src/ui/MaxwellUI.ui", NULL);
   
-  GObject *window = gtk_builder_get_object(builder, "window");
+  GObject *window = gtk_builder_get_object(builder, "Window");
   gtk_window_set_application(GTK_WINDOW(window), app);
   
   
-  GObject *canvas = gtk_builder_get_object(builder, "canvas");  
+  GObject *canvas = gtk_builder_get_object(builder, "Canvas");  
   g_timeout_add(33, G_SOURCE_FUNC(update_canvas), canvas);
 
-  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
+
+  gtk_widget_set_visible(GTK_WIDGET(window), true);
   g_object_unref(builder);   
 }
 
